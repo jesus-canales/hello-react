31a231c0d96ab5dbfcf1013f7df3d9b203cb6bfa
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-14 02:27:27 +0000

    Hemos trabajado con dos componentes

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -1,12 +1,15 @@
 import { Fragment } from "react"
 import HolaMundo from "./componentes/HolaMundo";
+import Mensaje from "./componentes/Mensaje";
 
 function App(){
 
   return (
     <>
       <HolaMundo />
+      <Mensaje />
       <HolaMundo />
+
     </>
   );
 
---
+++ HelloWorld/src/componentes/Mensaje.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+function Mensaje (){
+
+    return (
+        <>
+        <h1>Seguimos programando</h1>
+        <h4>React... hasta el infinito y más allá</h4>
+        </>
+    );
+}
+
+export default Mensaje;
