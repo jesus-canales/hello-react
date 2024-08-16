cbfd7fd2a9a9b3da2a5f4576e6c6bd59687e9436
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-16 01:31:54 +0000

    Hemos agregado el componente pie de página y hemos borrado los estilos de index.css

--- WebTest/src/App.jsx
+++ WebTest/src/App.jsx
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Titulo from './componentes/Titulo';
+import Pie from './componentes/Pie'
 
 function App (){
 
   return (
     <>
     <Titulo />
+    <Pie />
     </>
   );
 }
