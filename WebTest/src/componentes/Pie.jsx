88e0f706409ea00a702e1140e735816bdcd1a257
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-16 01:28:13 +0000

    Hemos creado el componente Pie de página

---
+++ WebTest/src/componentes/Pie.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+function Pie(){
+
+    return (
+        <>
+        <h4>TravelFAST</h4>
+        <h5>Expertos en recorrer el Perú</h5>
+        </>
+    );
+}
+
+export default Pie;
