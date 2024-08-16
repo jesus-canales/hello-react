be72c79870e061aec393aeb0bade6827cd0f1ff2
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-16 01:15:00 +0000

    He agregado el logo de cibertec

--- HelloWorld/src/App.jsx
+++ HelloWorld/src/App.jsx
@@ -9,7 +9,6 @@ function App(){
       <HolaMundo />
       <Mensaje />
       <HolaMundo />
-
     </>
   );
 
---
+++ WebTest/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ciberteclogo from './image/ciberteclogo.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://www.cibertec.edu.pe/" target="_blank">
+          <img src={ciberteclogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Jesús + Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click en los logos de Vite y React para saber más
+      </p>
+    </>
+  )
+}
+
+export default App
