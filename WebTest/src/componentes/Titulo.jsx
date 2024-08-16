9a024e993adcb7d81144ec33538b8212a40b6964
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-16 01:19:27 +0000

    He creado el componente titulo

---
+++ WebTest/src/componentes/Titulo.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+
+function Titulo (){
+
+    return (
+        <>
+        <h1>Conoce el Perú Primero</h1>
+        <h2>En el Perú existen hermososo lugares para conocer y disfrutar</h2>
+        </>
+    );
+}
+
+export default Titulo;
