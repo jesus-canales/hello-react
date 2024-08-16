ad6a706a49ac1b05847115c76922136d9d3bd263
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-16 01:44:56 +0000

    Hemos creado un componente con argumentos

---
+++ WebTest/src/componentes/Ciudad.jsx
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import './Estilos.css'
+
+function Ciudad ( {nombre, descripcion, imagen} ){
+
+    return (
+        <>
+        <div className="ciudad">
+            <h3 className="ciudad__titulo">{nombre}</h3>
+            <p className="ciudad__descripcion">{descripcion}</p>
+            <img src={imagen} alt={nombre} />
+        </div>
+        </>
+    );
+}
+
+export default Ciudad;
