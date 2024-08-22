8d97d5a4d91bd06e865a32871293c5cb0e9b3996
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-22 01:54:24 +0000

    He agregado un componente llamado acordion para las preguntas frecuentes

--- AppWeb/src/componentes/Contador.jsx
+++ AppWeb/src/componentes/Contador.jsx
@@ -1,21 +1,21 @@
 import { Fragment, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-function Contador(){
-    const [ contar, setContar] = useState(0);
+function Contador() {
+    const [contar, setContar] = useState(0);
     const hacerClic = () => {
-        setContar( contar + 1 );
+        setContar(contar + 1);
     }
     return (
         <>
-        <div className="container mt-3">
-        <div className="row justify-content-center">
-        <div className="col-md-auto">
-        <p>El número de click realizados es: { contar }</p>
-        <button onClick={ hacerClic }
-        type="button" className="btn btn-primary">Click aquí</button>
-        </div>
-        </div>
-        </div>
+            <div className="container mt-3">
+                <div className="row justify-content-center">
+                    <div className="col-md-auto">
+                        <p>El número de click realizados es: {contar}</p>
+                        <button onClick={hacerClic}
+                            type="button" className="btn btn-primary">Click aquí</button>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
