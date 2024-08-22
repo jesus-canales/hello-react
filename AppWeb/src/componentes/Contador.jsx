eca8c78833551c08301ee476380fb724ddfb6427
jesus-canales <177686251+jesus-canales@users.noreply.github.com>
2024-08-22 01:18:24 +0000

    He creado un componente contador bastante sencillo

---
+++ AppWeb/src/componentes/Contador.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function Contador(){
+    return (
+        <>
+        <p>Haz hecho click aquí 1 vez</p>
+        <button>Click aquí</button>
+        </>
+    );
+}
+
+export default Contador;
